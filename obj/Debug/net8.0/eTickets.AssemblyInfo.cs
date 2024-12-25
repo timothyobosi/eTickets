d6cb2d658c7580545819d8be227ffbda1f38771b
timothyobosi <timothyobosi8@gmail.com>
2024-12-26 01:45:34 +0300

    New commit

--- obj/Debug/net8.0/eTickets.AssemblyInfo.cs
+++ obj/Debug/net8.0/eTickets.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eTickets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f58195f55b96bd2fa72495cb198999ecb4c59e62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ccfdc8e4f3556015a400c8e1c7f238136338ddd")]
 [assembly: System.Reflection.AssemblyProductAttribute("eTickets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eTickets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
